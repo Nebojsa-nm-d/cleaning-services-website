ec7767d5734af6510b922a1c020cfd9377d0c2e8
nebojsa-mitrovic <32269043+Nebojsa-nm-d@users.noreply.github.com>
2020-11-18 07:02:43 +0100

    Contact Page styling

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,26 +1,37 @@
 // Slick Slider
-(function ($) {
-	$(document).ready(function () {
-		$(".sliders").slick({
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			arrows: true,
-			infinite: true,
-			speed: 500,
-			cssEase: "linear",
-			autoplay: false,
-			dots: false,
-			swipeToSlide: true,
-			autoplaySpeed: 7000,
-			fade: true,
-		});
+$( document ).ready(function() {
 
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 200) {
-				$("#main-nav").addClass("sticky-header");
-			} else {
-				$("#main-nav").removeClass("sticky-header");
-			}
-		});
-	});
-})(jQuery);
+  // Homepage Sliders
+  $(".sliders").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    cssEase: "linear",
+    autoplay: false,
+    dots: false,
+    swipeToSlide: true,
+    autoplaySpeed: 7000,
+    fade: true,
+  });
+
+  // Main Nav Sticky Header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $("#main-nav").addClass("sticky-header");
+    } else {
+      $("#main-nav").removeClass("sticky-header");
+    }
+  });
+  
+  // Contact page
+  $( '.contact-form__text-input' ).keyup(function() {
+    if($(this).val()) {
+      $(this).addClass('has-value');
+    } else {
+      $(this).removeClass('has-value');
+    }
+  });
+
+});
