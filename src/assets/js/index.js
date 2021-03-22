636611942fd132bdc342327fd029b82dbc8f4973
nebojsa-mitrovic <32269043+nebojsa-mitrovic@users.noreply.github.com>
2021-03-22 08:17:34 +0100

    Responsive menu

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,13 @@
-// Slick Slider
 $(function () {
+	const burgerMenu = document.getElementById("burger");
+	const navbarMenu = document.getElementById("menu");
+
+	// Responsive Navbar Toggle
+	burgerMenu.addEventListener("click", function () {
+		navbarMenu.classList.toggle("active");
+		burgerMenu.classList.toggle("active");
+	});
+
 	// Homepage Sliders
 	$(".sliders").slick({
 		slidesToShow: 1,
